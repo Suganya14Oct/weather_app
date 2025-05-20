9df39c9db9f206d416fc1901e8a673525f05c3db
Suganya.m <suganya.m@in.zlinkcorp.com>
2025-05-20 21:11:01 +0530

    ui

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'package:open_weather_provider/pages/search_page.dart';
 import 'package:open_weather_provider/provider/weather/weather_provider.dart';
 import 'package:open_weather_provider/repositories/weather_repository.dart';
 import 'package:open_weather_provider/services/weather_api_services.dart';
@@ -22,6 +23,13 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Weather'),
+        actions: [
+          IconButton(onPressed: () async {
+            await Navigator.push(context, MaterialPageRoute(builder: (context){
+              return SearchPage();
+            }));
+          }, icon: Icon(Icons.search))
+        ],
       ),
       body: Center(
         child: Text('Home'),
